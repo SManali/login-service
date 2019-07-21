fb7263f6d68344e22831cd3edde554076c8d17d7
SManali <sutar.manali@gmail.com>
2019-07-21 13:39:30 +0530

    express router

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const config = require('./config/config');
 const login = require('./routes/login');
 const signUp = require('./routes/sign-up');
 
 const app = express();
+
+//view engine set up
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 var mongoose = require('mongoose');
 mongoose.connect(`mongodb://${config.db_server}/${config.db_name}`);
